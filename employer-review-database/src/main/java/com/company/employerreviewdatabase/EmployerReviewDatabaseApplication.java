5b7f917d70de44848312608b8c61ac170197e2ae
Kyleah <krfrederic@gmail.com>
2020-08-24 14:18:29 -0500

    added HelloWorld

---
+++ employer-review-database/src/main/java/com/company/employerreviewdatabase/EmployerReviewDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.company.employerreviewdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployerReviewDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployerReviewDatabaseApplication.class, args);
+	}
+
+}
---
+++ employer-review-database/src/test/java/com/company/employerreviewdatabase/EmployerReviewDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.employerreviewdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployerReviewDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
