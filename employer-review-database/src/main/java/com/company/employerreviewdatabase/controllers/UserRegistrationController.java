1c9035e7d2d5461d7f50d53075d7bf1aede96ea0
Kyleah <krfrederic@gmail.com>
2020-09-09 17:54:42 -0500

    added hibernate

--- employer-review-database/src/main/java/com/company/employerreviewdatabase/controllers/UserRegistrationController.java
+++ employer-review-database/src/main/java/com/company/employerreviewdatabase/controllers/UserRegistrationController.java
@@ -1,6 +1,8 @@
 package com.company.employerreviewdatabase.controllers;
 
 import com.company.employerreviewdatabase.models.UserDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,16 +14,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "login")
 public class UserRegistrationController {
 
+    private static final Logger createLog = LoggerFactory.getLogger(UserRegistrationController.class);
+
     @GetMapping("register")
     public String registerUser(@ModelAttribute UserDTO userDTO, Model model) {
         model.addAttribute("user", userDTO);
         // TODO: 9/7/2020 add "register" into SecurityConfig .antMatchers, ensure is routed to proper html file
         return "login/register";
     }
-
+// when controller is called, creates a log storing the data inputted by user as a string
     @PostMapping("register")
-    public void save() {
-
+    public void save(UserDTO userDTO) {
+        createLog.info(">> userDTO : {}" + userDTO.toString());
     }
 
 }
