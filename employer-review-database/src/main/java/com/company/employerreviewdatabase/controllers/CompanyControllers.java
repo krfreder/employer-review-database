e248fd7bea85107b67f5a5df758b3d6391da5d5b
Kyleah <krfrederic@gmail.com>
2020-09-28 11:06:43 -0500

    AddController and add form created

---
+++ employer-review-database/src/main/java/com/company/employerreviewdatabase/controllers/CompanyControllers.java
@@ -0,0 +1,18 @@
+//package com.company.employerreviewdatabase.controllers;
+//
+//import com.company.employerreviewdatabase.respositories.CompanyRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class CompanyControllers extends AbstractBaseController {
+//
+//    @Autowired
+//    private CompanyRepository companyRepository;
+//
+//    @GetMapping("add")
+//    public String display
+//
+//
+//}
