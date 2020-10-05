65d89c970833f71e0746b5b120c3ad8b349667f6
Kyleah <krfrederic@gmail.com>
2020-10-05 06:59:41 -0500

    added controller and toSTring

--- employer-review-database/src/main/java/com/company/employerreviewdatabase/controllers/SearchController.java
+++ employer-review-database/src/main/java/com/company/employerreviewdatabase/controllers/SearchController.java
@@ -1,4 +1,40 @@
 package com.company.employerreviewdatabase.controllers;
 
+import com.company.employerreviewdatabase.models.Job;
+import com.company.employerreviewdatabase.models.Search;
+import com.company.employerreviewdatabase.respositories.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class SearchController {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @RequestMapping("search")
+    public String search(Model model) {
+        model.addAttribute("columns", columnChoices);
+        return "search";
+    }
+
+    @PostMapping("search/results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        Iterable<Job> jobs;
+        if (searchTerm.toLowerCase().equals("all") || searchTerm.equals("")){
+            jobs = jobRepository.findAll();
+        } else {
+            jobs = Search.findByColumnAndValue(searchType, searchTerm, jobRepository.findAll());
+        }
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
+
 }
