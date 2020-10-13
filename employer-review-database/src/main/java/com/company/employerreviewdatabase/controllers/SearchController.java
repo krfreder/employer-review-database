39efcb9a7d9ae09e88925e211935045c00c30245
Kyleah <krfrederic@gmail.com>
2020-10-13 07:56:45 -0500

    search fixed

--- employer-review-database/src/main/java/com/company/employerreviewdatabase/controllers/SearchController.java
+++ employer-review-database/src/main/java/com/company/employerreviewdatabase/controllers/SearchController.java
@@ -8,11 +8,12 @@ import com.company.employerreviewdatabase.respositories.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.HashMap;
+//import java.util.HashMap;
 
 @Controller
 @RequestMapping("search")
@@ -27,32 +28,41 @@ public class SearchController{
     @Autowired
     private IdentityRepository identityRepository;
 
-    static HashMap<String, String> columnChoices = new HashMap<>();
+//    static HashMap<String, String> columnChoices = new HashMap<>();
+//
+//    public SearchController() {
+//        columnChoices.put("all", "All");
+////        columnChoices.put("jobTitle", "Job Title");
+////        columnChoices.put("company", "Company");
+//        columnChoices.put("culture", "Culture");
+//        columnChoices.put("identity", "Identity");
+//    }
 
-    public SearchController() {
-        columnChoices.put("all", "All");
-//        columnChoices.put("jobTitle", "Job Title");
-//        columnChoices.put("company", "Company");
-        columnChoices.put("culture", "Culture");
-        columnChoices.put("identity", "Identity");
-    }
-
-    @RequestMapping("")
-    public String search(Model model) {
-        model.addAttribute("columns", columnChoices);
+    @GetMapping(value = "")
+    public String search() {
         return "search";
     }
 
+//    @PostMapping("results")
+//    public String displaySearchResults(Model model, @RequestParam String searchValue){
+//        Iterable<Job> jobs;
+//        if (searchValue.toLowerCase().equals("all") || searchTerm.equals("")){
+//            jobs = jobRepository.findAll();
+//        } else {
+//            jobs = Search.findByColumnAndValue(searchType, searchTerm, jobRepository.findAll());
+//        }
+//        model.addAttribute("columns", columnChoices);
+//        model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+//        model.addAttribute("jobs", jobs);
+//
+//        return "search";
+//    }
+
     @PostMapping("results")
-    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
-        Iterable<Job> jobs;
-        if (searchTerm.toLowerCase().equals("all") || searchTerm.equals("")){
-            jobs = jobRepository.findAll();
-        } else {
-            jobs = Search.findByColumnAndValue(searchType, searchTerm, jobRepository.findAll());
-        }
-        model.addAttribute("columns", columnChoices);
-        model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+    public String displaySearchResults(Model model, @RequestParam String searchValue){
+
+        Iterable<Job> jobs = Search.findSearchValue(searchValue, jobRepository.findAll());
+
         model.addAttribute("jobs", jobs);
 
         return "search";
--- employer-review-database/src/main/java/com/company/employerreviewdatabase/models/Search.java
+++ employer-review-database/src/main/java/com/company/employerreviewdatabase/models/Search.java
@@ -4,74 +4,97 @@ import java.util.ArrayList;
 
 public class Search {
 
-    public static ArrayList<Job> findByColumnAndValue(String column, String value, Iterable<Job> allJobs) {
+    public static ArrayList<Job> findSearchValue(String searchValue, Iterable<Job> allJobs) {
+
+        String lowerSearchVal = searchValue.toLowerCase();
 
         ArrayList<Job> results = new ArrayList<>();
 
-        if (value.toLowerCase().equals("all")){
-            return (ArrayList<Job>) allJobs;
-        }
-
-        if (column.equals("all")){
-            results = findByValue(value, allJobs);
-            return results;
-        }
         for (Job job : allJobs) {
-
-            String aValue = getFieldValue(job, column);
-
-            if (aValue != null && aValue.toLowerCase().contains(value.toLowerCase())) {
+            if(job.toString().toLowerCase().contains(lowerSearchVal)) {
                 results.add(job);
             }
+
+//            for(int i = 0; i < job.size; i++ )
+
+//            if (job.getJobTitle().toLowerCase().contains(lowerSearchVal)) {
+//                results.add(job);
+//            } else if (job.getCompany().toLowerCase().contains(lowerSearchVal)) {
+//                results.add(job);
+//            } else if (job.getCultures().toString().toLowerCase().contains(lowerSearchVal)) {
+//                results.add(job);
+//            } else if (job.getIdentities().toString().toLowerCase().contains(lowerSearchVal)) {
+//                results.add(job);
+//            } else if (job.toString().toLowerCase().contains(lowerSearchVal)) {
+//                results.add(job);
+//            }
+
         }
 
         return results;
     }
 
-    public static String getFieldValue(Job job, String fieldName){
-        String theValue;
-        if (fieldName.equals("jobTitle")){
-            theValue = job.getJobTitle();
-        } else if (fieldName.equals("company")) {
-            theValue = job.getCompany();
-        } else if (fieldName.equals("culture")) {
-            theValue = job.getCultures().toString();
-        } else {
-            theValue = job.getIdentities().toString();
-        }
+//    public static ArrayList<Job> findByColumnAndValue(String column, String value, Iterable<Job> allJobs) {
+//
+//        ArrayList<Job> results = new ArrayList<>();
+//
+//        if (value.toLowerCase().equals("all")){
+//            return (ArrayList<Job>) allJobs;
+//        }
+//
+//        if (column.equals("all")){
+//            results = findByValue(value, allJobs);
+//            return results;
+//        }
+//        for (Job job : allJobs) {
+//
+//            String aValue = getFieldValue(job, column);
+//
+//            if (aValue != null && aValue.toLowerCase().contains(value.toLowerCase())) {
+//                results.add(job);
+//            }
+//        }
+//
+//        return results;
+//    }
+//
+//    public static String getFieldValue(Job job, String fieldName){
+//        String theValue;
+//        if (fieldName.equals("jobTitle")){
+//            theValue = job.getJobTitle();
+//        } else if (fieldName.equals("company")) {
+//            theValue = job.getCompany();
+//        } else if (fieldName.equals("culture")) {
+//            theValue = job.getCultures().toString();
+//        } else {
+//            theValue = job.getIdentities().toString();
+//        }
+//
+//        return theValue;
+//    }
 
-        return theValue;
-    }
-
-        /**
-         * Search all Job fields for the given term.
-         *
-         * @param value The search term to look for.
-         * @param allJobs The list of jobs to search.
-         * @return      List of all jobs with at least one field containing the value.
-         */
-    public static ArrayList<Job> findByValue(String value, Iterable<Job> allJobs) {
-        String lower_val = value.toLowerCase();
-
-        ArrayList<Job> results = new ArrayList<>();
-
-        for (Job job : allJobs) {
-
-            if (job.getJobTitle().toLowerCase().contains(lower_val)) {
-                results.add(job);
-            } else if (job.getCompany().toLowerCase().contains(lower_val)) {
-                results.add(job);
-            } else if (job.getCultures().toString().toLowerCase().contains(lower_val)) {
-                results.add(job);
-            } else if (job.getIdentities().toString().toLowerCase().contains(lower_val)) {
-                results.add(job);
-            } else if (job.toString().toLowerCase().contains(lower_val)) {
-                results.add(job);
-            }
-
-        }
-
-        return results;
-    }
+    //    public static ArrayList<Job> findByValue(String value, Iterable<Job> allJobs) {
+//        String lower_val = value.toLowerCase();
+//
+//        ArrayList<Job> results = new ArrayList<>();
+//
+//        for (Job job : allJobs) {
+//
+//            if (job.getJobTitle().toLowerCase().contains(lower_val)) {
+//                results.add(job);
+//            } else if (job.getCompany().toLowerCase().contains(lower_val)) {
+//                results.add(job);
+//            } else if (job.getCultures().toString().toLowerCase().contains(lower_val)) {
+//                results.add(job);
+//            } else if (job.getIdentities().toString().toLowerCase().contains(lower_val)) {
+//                results.add(job);
+//            } else if (job.toString().toLowerCase().contains(lower_val)) {
+//                results.add(job);
+//            }
+//
+//        }
+//
+//        return results;
+//    }
 
 }
